4f19a8df83fabddcaa71d258516cd6f98573a337
JohnPolson <JP@JPs-MacBook-Pro.local>
2019-02-07 09:11:49 +0000

    Q7

---
+++ models/house.rb
@@ -0,0 +1,49 @@
+require_relative('../db/sql_runner')
+
+class House
+
+ attr_reader :name, :id
+
+  def initialize(house)
+    @id = house['id'].to_i if house['id']
+    @name = house['name']
+  end
+
+  def save()
+    sql = "INSERT INTO houses
+    (name) VALUES ($1)
+    RETURNING *"
+    values = [@name]
+    result = SqlRunner.run(sql, values)
+    @id = result.first()['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM houses"
+    houses = SqlRunner.run( sql )
+    result = houses.map { |house| House.new( house ) }
+    return result
+  end
+
+  def self.find( id )
+    sql = "SELECT * FROM houses
+    WHERE id = $1"
+    values = [id]
+    student = SqlRunner.run( sql, values )
+    result = House.new( house.first )
+    return result
+  end
+
+  def delete()
+    sql = "DELETE FROM houses
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run( sql, values )
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM houses;"
+    SqlRunner.run(sql)
+  end
+
+end
